f5be6350664f6840b1ce65294eea236ac0ee4bf9
Simon Edmunds <sjedmunds04@gmail.com>
2025-01-30 14:11:48 -0600

    Implemented caesar encoding/decoding

---
+++ caesar.py
@@ -0,0 +1,23 @@
+def caesar_encode_decode(plaintext: str, offset: int, decoding=False):
+    ciphertext = ""
+    offset = -offset if decoding else offset
+    if offset < 0:
+        offset = (offset + 26) % 26
+    for character in plaintext.upper():
+        if character.isalpha():
+            relative_ord = (ord(character) - ord("A") + offset) % 26
+            ciphertext += chr(ord("A") + relative_ord)
+        else:
+            ciphertext += character
+    return ciphertext
+
+
+if __name__ == '__main__':
+    # plaintext = input("Enter plain text: ")
+    # shift = input("Enter shift value: ")
+    plaintext = "Simon"
+    shift = 1
+    ciphertext = caesar_encode_decode(plaintext, int(shift))
+    print("Plain text:", plaintext)
+    print("Cipher text:", ciphertext)
+    print("Decoded text:", caesar_encode_decode(ciphertext, int(shift), decoding=True))
