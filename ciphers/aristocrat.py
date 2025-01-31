08e22f1bdae93c49f1a4ddd4433e8a1205470338
Simon Edmunds <sjebkc@umsystem.edu>
2025-01-30 18:24:04 -0600

    Cleaning up

--- ciphers/aristocrat.py
+++ ciphers/aristocrat.py
@@ -16,10 +16,12 @@ def aristocrat_encode_decode(plaintext: str, mapping: str, decoding=False):
 
 
 if __name__ == '__main__':
-    # Creating some basic plaintext to encode
+    # Creating some basic plaintext and using a mapping to encode
     plaintext = "The quick brown fox jumps over the lazy dog"
     mapping = "QWERTYUIOPASDFGHJKLZXCVBNM"
     ciphertext = aristocrat_encode_decode(plaintext, mapping)
     print("Plain Text:", plaintext)
     print("Cipher Text:", ciphertext)
+
+    # Decoding and printing output
     print("Decoded Text:", aristocrat_encode_decode(ciphertext, mapping, True))
