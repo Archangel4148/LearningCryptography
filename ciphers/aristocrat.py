09f40d701d13978c530308ece289d7c88ac5eda6
Simon Edmunds <sjebkc@umsystem.edu>
2025-01-30 22:26:39 -0600

    Implemented Aristocrat Cipher using new framework

--- ciphers/aristocrat.py
+++ ciphers/aristocrat.py
@@ -1,4 +1,32 @@
-from cryptography_resources import ALPHABET
+from cryptography_resources import ALPHABET, Cipher
+
+
+class AristocratCipher(Cipher):
+    def __init__(self, text: str):
+        super().__init__(text)
+        self.mapping = ""
+
+    def encrypt(self, text=None):
+        if text is None:
+            text = self.text
+        ciphertext = ""
+        for c in text.upper():
+            if c.isalpha():
+                ciphertext += self.mapping[ALPHABET.index(c)]
+            else:
+                ciphertext += c
+        return ciphertext
+
+    def decrypt(self, text=None):
+        if text is None:
+            text = self.text
+        plaintext = ""
+        for c in text.upper():
+            if c.isalpha():
+                plaintext += ALPHABET[self.mapping.index(c)]
+            else:
+                plaintext += c
+        return plaintext
 
 
 def aristocrat_encode_decode(plaintext: str, mapping: str, decoding=False):
@@ -17,11 +45,12 @@ def aristocrat_encode_decode(plaintext: str, mapping: str, decoding=False):
 
 if __name__ == '__main__':
     # Creating some basic plaintext and using a mapping to encode
-    plaintext = "The quick brown fox jumps over the lazy dog"
-    mapping = "QWERTYUIOPASDFGHJKLZXCVBNM"
-    ciphertext = aristocrat_encode_decode(plaintext, mapping)
-    print("Plain Text:", plaintext)
+    aristocrat = AristocratCipher("The quick brown fox jumps over the lazy dog")
+    aristocrat.mapping = "QWERTYUIOPASDFGHJKLZXCVBNM"
+
+    print("Plain Text:", aristocrat.text)
+    ciphertext = aristocrat.encrypt()
     print("Cipher Text:", ciphertext)
 
     # Decoding and printing output
-    print("Decoded Text:", aristocrat_encode_decode(ciphertext, mapping, True))
+    print("Decoded Text:", aristocrat.decrypt(ciphertext))
