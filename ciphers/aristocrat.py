c09a1c09885f5b3ae425a97c53612418ce707b6c
Simon Edmunds <sjedmunds04@gmail.com>
2025-02-04 09:48:52 -0600

    Starting aristocrat cracking

--- ciphers/aristocrat.py
+++ ciphers/aristocrat.py
@@ -1,4 +1,4 @@
-from cryptography_resources import ALPHABET, Cipher
+from cryptography_resources import ALPHABET, Cipher, english_letter_frequency
 
 
 class AristocratCipher(Cipher):
@@ -29,6 +29,24 @@ class AristocratCipher(Cipher):
         return plaintext
 
 
+def crack_mapping(ciphertext: str):
+    ciphertext = ''.join([c.upper() for c in ciphertext if c.isalpha()])
+    # Count the frequency of characters in the ciphertext
+    cipher_counts = Counter(ciphertext)
+
+    # Sort the characters by frequency
+    cipher_frequency = ''.join([item[0] for item in cipher_counts.most_common()])
+
+    # Create a mapping based on frequency
+    mapping = {cipher_frequency[i]: list(english_letter_frequency)[i] for i in range(len(cipher_frequency))}
+
+    # Now we use this mapping to decrypt the ciphertext
+    decrypted_text = ''.join([mapping.get(c, c) for c in ciphertext])
+
+    # Return the mapping and the decoded text (for testing purposes)
+    return decrypted_text, mapping
+
+
 if __name__ == '__main__':
     # Creating some basic plaintext and using a mapping to encode
     aristocrat = AristocratCipher("The quick brown fox jumps over the lazy dog")
@@ -38,5 +56,9 @@ if __name__ == '__main__':
     ciphertext = aristocrat.encrypt()
     print("Cipher Text:", ciphertext)
 
+    decryption, mapping = crack_mapping(ciphertext)
+    # Printing cracked output
+    print(mapping, "\n", decryption)
+
     # Decoding and printing output
-    print("Decoded Text:", aristocrat.decrypt(ciphertext))
+    # print("Decoded Text:", aristocrat.decrypt(ciphertext))
